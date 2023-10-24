6208443a14264ce44badb12b7bd2833137d4d557
Shanu Flash <shanuflash@gmail.com>
2023-10-24 23:55:40 +0530

    test1

--- src/app/game/[id]/player1/page.jsx
+++ src/app/game/[id]/player1/page.jsx
@@ -15,6 +15,7 @@ const player1 = ({ params }) => {
 
   const handleScan = (result) => {
     if (result) {
+      console.log(result);
       setResult(result.data);
     }
   };
@@ -23,29 +24,29 @@ const player1 = ({ params }) => {
     toast.error(error);
   };
 
-  useEffect(() => {
-    // @source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
-    let hasTouchScreen = false;
-    if ("maxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.maxTouchPoints > 0;
-    } else if ("msMaxTouchPoints" in navigator) {
-      hasTouchScreen = navigator.msMaxTouchPoints > 0;
-    } else {
-      const mQ = matchMedia?.("(pointer:coarse)");
-      if (mQ?.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-      } else if ("orientation" in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-      } else {
-        // Only as a last resort, fall back to user agent sniffing
-        const UA = navigator.userAgent;
-        hasTouchScreen =
-          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-          /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
-      }
-    }
-    setIsMobile(hasTouchScreen);
-  }, []);
+  // useEffect(() => {
+  //   // @source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
+  //   let hasTouchScreen = false;
+  //   if ("maxTouchPoints" in navigator) {
+  //     hasTouchScreen = navigator.maxTouchPoints > 0;
+  //   } else if ("msMaxTouchPoints" in navigator) {
+  //     hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  //   } else {
+  //     const mQ = matchMedia?.("(pointer:coarse)");
+  //     if (mQ?.media === "(pointer:coarse)") {
+  //       hasTouchScreen = !!mQ.matches;
+  //     } else if ("orientation" in window) {
+  //       hasTouchScreen = true; // deprecated, but good fallback
+  //     } else {
+  //       // Only as a last resort, fall back to user agent sniffing
+  //       const UA = navigator.userAgent;
+  //       hasTouchScreen =
+  //         /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+  //         /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+  //     }
+  //   }
+  //   setIsMobile(hasTouchScreen);
+  // }, []);
 
   if (!isMobile) {
     return (
