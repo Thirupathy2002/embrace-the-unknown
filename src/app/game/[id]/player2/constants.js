fd3b269b3901737a0d62b6d6bc62db7fa5861c75
Gokul Nath A <gokulnath.a@surveysparrow.com>
2023-10-25 00:27:39 +0530

    player 2 code editor

---
+++ src/app/game/[id]/player2/constants.js
@@ -0,0 +1,64 @@
+export const langOptions = [
+  { value: "javascript", label: "JavaScript" },
+  { value: "python", label: "Python" },
+  { value: "java", label: "Java" },
+  { value: "c", label: "C" },
+  { value: "cpp", label: "C++" },
+]
+
+export const initialCode = {
+  javascript: `function add(a, b) { 
+  return a + b;
+}`,
+
+  python: `def add(a, b):
+  return a + b`,
+
+  c: `int add(int a, int b) {
+    return a + b;
+}
+`,
+
+  cpp: `int add(int a, int b) {
+    return a + b;
+}
+`,
+
+  java: `public class Main {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = add(5, 3);
+        System.out.println(result);
+    }
+}`,
+}
+
+export const selectStyles = {
+  control: (baseStyles, { isFocused }) => ({
+    ...baseStyles,
+    width: "20%",
+    backgroundColor: "#161B22",
+    color: "#dadada",
+    margin: "5px 0",
+    cursor: "pointer",
+    border: "none",
+    borderColor: isFocused ? "#dadada" : "#161B22",
+  }),
+  option: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: "#161B22",
+    color: "#dadada",
+  }),
+  menuList: (baseStyles) => ({
+    ...baseStyles,
+    backgroundColor: "#161B22",
+    color: "#dadada",
+  }),
+  singleValue: (baseStyles) => ({
+    ...baseStyles,
+    color: "#dadada",
+  }),
+}
--- src/app/game/[id]/player2/page.jsx
+++ src/app/game/[id]/player2/page.jsx
@@ -1,12 +1,60 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react"
 import styles from "@/styles/game.module.scss"
+import Select from "react-select"
+import { langOptions, selectStyles, initialCode } from "./constants"
+import CodeMirror from "@uiw/react-codemirror"
+import { loadLanguage } from "@uiw/codemirror-extensions-langs"
+import { githubDark } from "@uiw/codemirror-theme-github"
 
 const Player2 = () => {
-  const [playingStatus, setPlayingStatus] = useState(false)
+  const [playingStatus, setPlayingStatus] = useState(true)
+  const [lang, setLang] = useState("javascript")
+  const [code, setCode] = useState(initialCode[lang])
+  const handleSelectChange = (selectLang) => {
+    setLang(selectLang.value)
+  }
+
+  const handleCodeChange = (code) => {
+    setCode(code)
+  }
+
+  useEffect(() => {
+    setCode(initialCode[lang])
+  }, [lang])
+
   return (
     <main className={styles.container}>
-      {!playingStatus ? <h2 className=''>Wait Untill Player 1 Completes his challenge</h2> : <h2>Code Editor</h2>}
+      {!playingStatus ? (
+        <h2 className={styles.title}>
+          Wait Untill Player 1 Completes his challenge
+        </h2>
+      ) : (
+        <>
+          <h2 className={styles.title}>Your Coding Challenge</h2>
+          <CodeMirror
+            value={code}
+            theme={githubDark}
+            minHeight="300px"
+            maxHeight="400px"
+            extensions={[loadLanguage(lang)]}
+            placeholder={`Enter your ${lang} code`}
+            onChange={handleCodeChange}
+          />
+          <Select
+            classNamePrefix="select"
+            defaultValue={langOptions[0]}
+            isSearchable={true}
+            onChange={handleSelectChange}
+            name="lang"
+            options={langOptions}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
+            styles={selectStyles}
+          />
+        </>
+      )}
     </main>
   )
 }
