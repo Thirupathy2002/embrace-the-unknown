a916050060ca627f4418e5314d5c831d24be0a1c
Shanu Flash <shanuflash@gmail.com>
2023-10-24 22:51:58 +0530

    mobile detection

--- src/app/game/[id]/player1/page.jsx
+++ src/app/game/[id]/player1/page.jsx
@@ -1,24 +1,82 @@
-"use client"
-import React, { useState } from "react"
-import styles from "@/styles/game.module.scss"
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "@/styles/game.module.scss";
+import QrReader from "reactjs-qr-reader";
+import { toast } from "sonner";
 
+const player1 = ({ params }) => {
+  const roomID = params.id;
+  const delay = 500;
 
-const player1 = () => {
-  const [player1Playing, setplayer1Playing] = useState(true)
+  const previewStyle = {};
+
+  const [result, setResult] = useState("No result");
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleScan = (result) => {
+    if (result) {
+      setResult(result.data);
+    }
+  };
+
+  const handleError = (error) => {
+    toast.error(error);
+  };
+
+  useEffect(() => {
+    // @source: https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#mobile_device_detection
+    let hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+      hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    } else {
+      const mQ = matchMedia?.("(pointer:coarse)");
+      if (mQ?.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+      } else if ("orientation" in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+      } else {
+        // Only as a last resort, fall back to user agent sniffing
+        const UA = navigator.userAgent;
+        hasTouchScreen =
+          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+          /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+      }
+    }
+    setIsMobile(hasTouchScreen);
+  }, []);
+
+  if (!isMobile) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.head}>
+          <div className={styles.title}>EMBRACE THE UNKNOWN</div>
+          Room ID: <span>{roomID}</span>
+          <br />
+          Make sure you and your teammate are on the same room.
+          <br />
+          <div className={styles.notmobile}>⚠️ Player 1 must be using a mobile device.</div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <main className={styles.container}>
-      <h1>Your Puzzle to solve</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, vero ad?
-        Dignissimos labore expedita amet ex consequuntur sit magni deserunt
-        earum cupiditate? Illo, quis qui laboriosam sequi omnis autem eos
-        reprehenderit soluta dignissimos ut deleniti iste, iusto earum atque
-        ipsum nostrum, explicabo impedit? Quasi veritatis consequuntur voluptas
-        at molestias distinctio expedita animi odio, vero sapiente nihil
-        perspiciatis blanditiis dolores officiis!
-      </p>
-    </main>
-  )
-}
+    <div className={styles.main}>
+      <div className={styles.head}>
+        <div className={styles.title}>EMBRACE THE UNKNOWN</div>
+        Room ID: <span>{roomID}</span>
+        <br />
+        Make sure you and your teammate are on the same room.
+        <br />
+        Scan the QR code to reveal a puzzle to solve.
+      </div>
+      <div style={{ width: "100vw", height: "100vw" }}>
+        <QrReader delay={delay} onError={handleError} onScan={handleScan} />
+      </div>
+      <p>{result}</p>
+    </div>
+  );
+};
 
-export default player1
+export default player1;
